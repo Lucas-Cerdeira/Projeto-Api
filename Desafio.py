d131a83dcb90999fff71ef3a3dc4ff67ecdd3c88
Lucas-Cerdeira <cerdeiralucas4121@gmail.com>
2022-07-14 17:51:15 -0300

    Projeto-Api

---
+++ Desafio.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+class Usuario(BaseModel):
+    nome: str
+    idade: int
+
+app = FastAPI()
+
+
+BANCO_DE_DADOS = []
+
+
+@app.post("/Pessoa")
+def add_pessoa(usuario: Usuario):
+    BANCO_DE_DADOS.append(usuario)
+    with open('Lista_Pessoas.txt', 'a') as arquivo:
+        arquivo.write(str(usuario) + '\n')
+        return str(usuario)
+
+
+
+
+@app.get("/Pessoas")
+def ver_pessoas():
+    with open('Lista_Pessoas.txt', 'r') as arquivo:
+        BANCO_DE_DADOS.clear()
+        for pessoas in arquivo:
+            BANCO_DE_DADOS.append(pessoas)
+
+        return BANCO_DE_DADOS
+
+
+
